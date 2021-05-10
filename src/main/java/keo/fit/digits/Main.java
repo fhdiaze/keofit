44fc7d206aa28a96f664dcdd283a0090dc98f28f
Fredy Diaz <fhdiaz87@gmail.com>
2021-05-10 10:01:26 -0500

    Fist commit

---
+++ src/main/java/keo/fit/digits/Main.java
@@ -0,0 +1,67 @@
+package keo.fit.digits;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Main s = new Main();
+        s.execute();
+    }
+
+    private void execute() throws FileNotFoundException {
+        File file = new File("D:/Work/Keo/Code/Fit/src/main/java/keo/fit/digits/input.txt");
+        Scanner scanner = new Scanner(file);
+
+        String line = scanner.nextLine();
+        String[] fields = line.split(" ");
+        int length = Integer.parseInt(fields[0]);
+        int sum = Integer.parseInt(fields[1]);
+
+        Problem problem = new Problem();
+        String[] solution = problem.solve(length, sum);
+
+        System.out.println(String.join(" ", solution));
+
+        scanner.close();
+    }
+
+    private class Problem {
+        public String[] solve(int length, int targetSum) {
+            String max, min;
+            int nines = targetSum / 9;
+            int lastDigit = targetSum % 9;
+            int digits = nines + (lastDigit > 0 ? 1 : 0);
+
+            if (digits <= length && targetSum > 0) {
+                StringBuilder builder = new StringBuilder(length);
+                builder.append("9".repeat(nines));
+
+                if (lastDigit > 0) {
+                    builder.append(lastDigit);
+                }
+
+                int padding = length - digits;
+                String maxPad = "0".repeat(padding);
+                String minPad = maxPad.replaceFirst("0", "1");
+
+                max = builder.toString().concat(maxPad);
+
+                if (padding > 0) {
+                    builder = builder.reverse();
+                    int digit = Character.getNumericValue(builder.charAt(0)) - 1;
+                    min = minPad + builder.replace(0, 1, String.valueOf(digit)).toString();
+                } else {
+                    min = builder.reverse().toString();
+                }
+            } else {
+                max = "-1";
+                min = "-1";
+            }
+
+            return new String[] { min, max };
+        }
+    }
+}
---
+++ src/main/java/keo/fit/market/Main.java
@@ -0,0 +1,103 @@
+package keo.fit.market;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Main s = new Main();
+        s.execute();
+    }
+
+    private void execute() throws FileNotFoundException {
+        File file = new File("D:/Work/Keo/Code/Fit/src/main/res/input.txt");
+        Scanner scanner = new Scanner(file);
+
+        int cases = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < cases; i++) {
+            int objects = Integer.parseInt(scanner.nextLine());
+            Item[] items = new Item[objects];
+
+            for (int j = 0; j < objects; j++) {
+                String line = scanner.nextLine();
+                String[] fields = line.split(" ");
+                items[j] = new Item(Integer.parseInt(fields[0]), Integer.parseInt(fields[1]));
+            }
+
+            int people = Integer.parseInt(scanner.nextLine());
+            int[] capacities = new int[people];
+
+            for (int p = 0; p < people; p++) {
+                capacities[p] = Integer.parseInt(scanner.nextLine());
+            }
+
+            Problem problem = new Problem(items, capacities);
+            int solution = problem.solve();
+
+            System.out.println(solution);
+        }
+
+        scanner.close();
+    }
+
+    private class Item {
+        private int price;
+        private int weight;
+
+        public Item(int price, int weight) {
+            this.price = price;
+            this.weight = weight;
+        }
+
+        public int getPrice() {
+            return this.price;
+        }
+
+        public int getWeight() {
+            return this.weight;
+        }
+
+        public double getRate() {
+            return price * 1.0 / weight;
+        }
+    }
+
+    private class Problem {
+        private Item[] objects;
+        private int[] capacities;
+
+        public Problem(Item[] objects, int[] capacities) {
+            Arrays.sort(objects, Comparator.comparing(Item::getRate).reversed());
+            this.objects = objects;
+            this.capacities = capacities;
+        }
+
+        public int solve() {
+            int totalWeight = 0;
+
+            for (int c : capacities) {
+                totalWeight += this.loadPerson(objects, c);
+            }
+
+            return totalWeight;
+        }
+
+        public int loadPerson(Item[] objects, int capacity) {
+            int value = 0;
+
+            for (Item o : objects) {
+                if (o.getWeight() <= capacity) {
+                    value += o.getPrice();
+                    capacity -= o.getWeight();
+                }
+            }
+
+            return value;
+        }
+    }
+}
